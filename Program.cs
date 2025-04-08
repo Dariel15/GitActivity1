b36e72d30fa5b043c2041f93d125ba455fb0f225
Dariel15 <darieltaberara@gmail.com>
2025-04-08 17:13:14 +0800

    Added Subtraction function

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ class Program
     {
         Console.WriteLine("Hello! This is my initial program");
         Add(3, 6);
+        Subtract(23, 12);
     }
 
     static void Add(int x, int y)
     {
         Console.WriteLine($"\n{x} + {y}");
         Console.WriteLine($"Sum: {x + y}");
+    }
+
+    static void Subtract(int x, int y)
+    {
+        Console.WriteLine($"\n{x} - {y}");
+        Console.WriteLine($"Difference: {x - y}");
     }
 }
